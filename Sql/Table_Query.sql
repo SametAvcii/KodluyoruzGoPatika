16823fe9e9a63999636942fb454017257a79e53e
sametavcii <sametavc05@gmail.com>
2022-02-19 14:57:38 +0300

    INSERT, DELETE, UPDATE işlemleri yapıldı

---
+++ Sql/Table_Query.sql
@@ -0,0 +1,101 @@
+CREATE TABLE employee 
+(
+	id INTEGER PRIMARY KEY,
+	name varchar(50),
+	birthday date,
+	email varchar(100)
+);
+
+//Burada employee adında bir tablo ve id ,name,birthday ve email sütünlarını oluşturduk.
+
+insert into employe (name, email, birthday) values ('Pammi', 'pputtrell0@diigo.com', '2021-08-27');
+insert into employe (name, email, birthday) values ('Becka', 'bpalffy1@tinyurl.com', '2022-01-25');
+insert into employe (name, email, birthday) values ('Theo', 'tcrockford2@ucsd.edu', '2021-11-06');
+insert into employe (name, email, birthday) values ('Sonnie', 'svosper3@blogtalkradio.com', '2021-10-20');
+insert into employe (name, email, birthday) values ('Den', 'dpepys4@parallels.com', '2021-08-12');
+insert into employe (name, email, birthday) values ('Sunshine', 'ssears5@desdev.cn', '2021-11-30');
+insert into employe (name, email, birthday) values ('Joachim', 'jmoulder6@vinaora.com', '2021-05-15');
+insert into employe (name, email, birthday) values ('Cicely', 'cromke7@barnesandnoble.com', '2021-04-27');
+insert into employe (name, email, birthday) values ('Sena', 'sneljes8@tmall.com', '2022-02-10');
+insert into employe (name, email, birthday) values ('Silvana', 'sgotthard9@blogger.com', '2021-12-30');
+insert into employe (name, email, birthday) values ('Christiana', 'challana@studiopress.com', '2022-02-18');
+insert into employe (name, email, birthday) values ('Garrick', 'golekhovb@ask.com', '2021-09-02');
+insert into employe (name, email, birthday) values ('Maurise', 'mlowaterc@buzzfeed.com', '2021-07-17');
+insert into employe (name, email, birthday) values ('Sonny', 'sstrognelld@123-reg.co.uk', '2021-12-22');
+insert into employe (name, email, birthday) values ('Denyse', 'dbrideauxe@yolasite.com', '2021-04-11');
+insert into employe (name, email, birthday) values ('Wilhelm', 'wnootf@mit.edu', '2021-05-19');
+insert into employe (name, email, birthday) values ('Kaja', 'kmussettg@sun.com', '2021-12-02');
+insert into employe (name, email, birthday) values ('Sibby', 'salvarh@addthis.com', '2021-07-25');
+insert into employe (name, email, birthday) values ('Bernadene', 'bharrelli@eepurl.com', '2021-08-10');
+insert into employe (name, email, birthday) values ('Norma', 'nlesekj@discuz.net', '2021-04-29');
+insert into employe (name, email, birthday) values ('Urban', 'umulhallk@mayoclinic.com', '2022-01-19');
+insert into employe (name, email, birthday) values ('Jasper', 'jgridleyl@163.com', '2022-02-10');
+insert into employe (name, email, birthday) values ('Lavina', 'lmatejm@icio.us', '2022-01-06');
+insert into employe (name, email, birthday) values ('Rosetta', 'rcampanan@squidoo.com', '2021-08-02');
+insert into employe (name, email, birthday) values ('Rosene', 'rsancheso@tripadvisor.com', '2022-01-13');
+insert into employe (name, email, birthday) values ('Teddy', 'tdeantonip@qq.com', '2021-03-30');
+insert into employe (name, email, birthday) values ('Tarah', 'tcattiniq@blogger.com', '2021-03-21');
+insert into employe (name, email, birthday) values ('Babette', 'bcaudrayr@goo.ne.jp', '2021-09-26');
+insert into employe (name, email, birthday) values ('Janeva', 'jaldgates@com.com', '2021-11-09');
+insert into employe (name, email, birthday) values ('Meryl', 'mmakent@google.com.br', '2021-05-30');
+insert into employe (name, email, birthday) values ('Sam', 'shighnamu@myspace.com', '2022-02-01');
+insert into employe (name, email, birthday) values ('Nicola', 'ncammidgev@ebay.com', '2021-02-24');
+insert into employe (name, email, birthday) values ('Bree', 'bschulzw@blogger.com', '2021-04-26');
+insert into employe (name, email, birthday) values ('Kermit', 'kgoudgex@skyrock.com', '2021-05-22');
+insert into employe (name, email, birthday) values ('Skell', 'saustwicky@infoseek.co.jp', '2021-05-31');
+insert into employe (name, email, birthday) values ('Marillin', 'mmarmonz@google.ca', '2021-09-10');
+insert into employe (name, email, birthday) values ('Tildie', 'tsiss10@wired.com', '2021-09-09');
+insert into employe (name, email, birthday) values ('Rubi', 'rmccoughan11@posterous.com', '2021-03-27');
+insert into employe (name, email, birthday) values ('Judas', 'jtolson12@cbslocal.com', '2021-04-12');
+insert into employe (name, email, birthday) values ('Lebbie', 'lkoppeck13@dailymotion.com', '2021-05-12');
+insert into employe (name, email, birthday) values ('Jessey', 'jdomel14@deliciousdays.com', '2021-06-06');
+insert into employe (name, email, birthday) values ('Flinn', 'frickford15@dell.com', '2021-04-03');
+insert into employe (name, email, birthday) values ('Lorne', 'lanscott16@vkontakte.ru', '2021-11-15');
+insert into employe (name, email, birthday) values ('Adah', 'akarpfen17@cbsnews.com', '2021-04-12');
+insert into employe (name, email, birthday) values ('Tiena', 'tpilipets18@lulu.com', '2021-10-26');
+insert into employe (name, email, birthday) values ('Tommie', 'tcarluccio19@cmu.edu', '2021-04-17');
+insert into employe (name, email, birthday) values ('Alex', 'apittman1a@barnesandnoble.com', '2021-10-04');
+insert into employe (name, email, birthday) values ('Tobe', 'ttuddall1b@wikia.com', '2021-12-18');
+insert into employe (name, email, birthday) values ('Andy', 'abofield1c@toplist.cz', '2021-12-20');
+insert into employe (name, email, birthday) values ('Maureen', 'mjaves1d@ebay.co.uk', '2021-07-28');
+
+//Mockaroo sitesinde oluşturulan verileri employe tablomuza ekledik.
+
+UPDATE employe
+SET name ='Avci',
+birthday ='2001-01-01',
+email='turkeyrep@turkey.com'
+WHERE email LIKE 's%';
+
+//Update işlemi yapıldı
+
+UPDATE employe
+SET name ='Avci',
+birthday ='2001-01-01',
+email='turkeyrep@turkey.com'
+WHERE id > 40;
+
+//Update işlemi yapıldı
+
+UPDATE employe
+SET name ='Avci',
+birthday ='2001-01-01',
+email='turkeyrep@turkey.com'
+WHERE name LIKE 'A%';
+
+//Update işlemi yapıldı
+
+DELETE FROM employe
+WHERE name LIKE 'J%';
+
+//İsmi J ile başlayan veriler silindi.
+
+DELETE FROM employe
+WHERE email LIKE 'b%';
+
+//email adresi b ile başlayan veriler silindi.
+
+DELETE FROM employe
+WHERE birthday = '2021/01/01';
+
+// Doğum tarihine göre Delete işlemi yapıldı
