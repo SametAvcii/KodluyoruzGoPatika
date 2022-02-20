e3c198b70aa2065587f5c1e4a6d6519707ab7241
sametavcii <sametavc05@gmail.com>
2022-02-20 12:56:11 +0300

    INNER JOIN sorguları yapıldı.

---
+++ Sql/INNER_JOIN.sql
@@ -0,0 +1,14 @@
+SELECT city, country FROM city
+INNER JOIN country ON city.country_id = country.country_id;
+
+//City tablosundaki şehirlerin  country tablosundaki ülkelerini beraber getiren INNER JOIN sorgusudur.
+
+SELECT payment_id ,first_name, last_name FROM customer
+INNER JOIN payment ON payment.customer_id = customer.customer_id;
+
+//Customer tablosundaki first_name ve last_name ile payment tablosundaki payment_id sütunlarını beraber getiren sorgudur.
+
+SELECT rental_id ,first_name, last_name FROM customer
+INNER JOIN rental ON rental.customer_id = customer.customer_id;
+
+////Customer tablosundaki first_name ve last_name ile rental tablosundaki rental_id sütunlarını beraber getiren sorgudur.
